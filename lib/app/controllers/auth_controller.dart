2ff814d54e4de542b690844472027f255a96e170
MexicianDepartementDefense <faisal216093@gmail.com>
2024-05-31 11:00:11 +0700

    test

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: unnecessary_overrides
-
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {
-
+class AuthController extends GetxController {
+  //TODO: Implement AuthController
 
   final count = 0.obs;
   @override
---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unnecessary_overrides
+
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  TextEditingController nama = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+}
