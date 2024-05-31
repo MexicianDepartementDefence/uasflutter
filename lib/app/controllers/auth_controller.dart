6b5290cd41b411ff7cad410ce8faefbfdccbda91
MexicianDepartementDefense <faisal216093@gmail.com>
2024-05-31 11:02:43 +0700

    text

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: unnecessary_overrides
+
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
-  //TODO: Implement AuthController
 
   final count = 0.obs;
   @override
